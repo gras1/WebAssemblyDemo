1735f8b8b1b28e32318de3d36c83d6068aaa6e8c
gras1 <graham.fearn@googlemail.com>
2019-04-29 00:24:11 +0100

    Update index11.js

--- index11.js
+++ index11.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
-const buf = fs.readFileSync("./fib.wasm");
+const buf = fs.readFileSync("./tableexample2.wasm");
 const wasmModule = new WebAssembly.Module(new Uint8Array(buf));
 
-const wasmInstance = new WebAssembly.Instance(wasmModule);
+let tbl = new WebAssembly.Table({initial:2, element:"anyfunc"});
+var importObject = {
+    js: {
+        table: tbl
+    }
+};
 
-console.log(wasmInstance.exports.fib(7));
+const wasmInstance = new WebAssembly.Instance(wasmModule, importObject);
+
+console.log(tbl.get(0)());
+console.log(tbl.get(1)());
