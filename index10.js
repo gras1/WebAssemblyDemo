9585dc77b5312151444e09c436c6ed2a7b4809db
gras1 <graham.fearn@googlemail.com>
2019-04-29 00:25:17 +0100

    Update index10.js

--- index10.js
+++ index10.js
@@ -1,15 +1,8 @@
 const fs = require("fs");
-const buf = fs.readFileSync("./tableexample2.wasm");
+const buf = fs.readFileSync("./tableexample.wasm");
 const wasmModule = new WebAssembly.Module(new Uint8Array(buf));
 
-let tbl = new WebAssembly.Table({initial:2, element:"anyfunc"});
-var importObject = {
-    js: {
-        table: tbl
-    }
-};
+const wasmInstance = new WebAssembly.Instance(wasmModule);
 
-const wasmInstance = new WebAssembly.Instance(wasmModule, importObject);
-
-console.log(tbl.get(0)());
-console.log(tbl.get(1)());
+console.log(wasmInstance.exports.callByIndex(0));
+console.log(wasmInstance.exports.callByIndex(1));
