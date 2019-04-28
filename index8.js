e0e02aa434fe641e7371c9505641736bb182c8d2
gras1 <graham.fearn@googlemail.com>
2019-04-29 00:27:42 +0100

    Update index8.js

--- index8.js
+++ index8.js
@@ -2,14 +2,7 @@ const fs = require("fs");
 const buf = fs.readFileSync("./usingglobal.wasm");
 const wasmModule = new WebAssembly.Module(new Uint8Array(buf));
 
-let myGlobal = new WebAssembly.Global({value:'i32', mutable:true}, 0);
-var importObject = {
-    js: {
-        global: myGlobal
-    }
-};
-
-const wasmInstance = new WebAssembly.Instance(wasmModule, importObject);
+const wasmInstance = new WebAssembly.Instance(wasmModule);
 
 wasmInstance.exports.incGlobal();
 wasmInstance.exports.incGlobal();
