16897ccac9a7e6679aa13913592c748e6e323267
gras1 <graham.fearn@googlemail.com>
2019-04-29 19:59:18 +0100

    Updated name of incGlobal to incrementGlobal

--- index8.js
+++ index8.js
@@ -4,8 +4,8 @@ const wasmModule = new WebAssembly.Module(new Uint8Array(buf));
 
 const wasmInstance = new WebAssembly.Instance(wasmModule);
 
-wasmInstance.exports.incGlobal();
-wasmInstance.exports.incGlobal();
-wasmInstance.exports.incGlobal();
-wasmInstance.exports.incGlobal();
+wasmInstance.exports.incrementGlobal();
+wasmInstance.exports.incrementGlobal();
+wasmInstance.exports.incrementGlobal();
+wasmInstance.exports.incrementGlobal();
 console.log(wasmInstance.exports.getGlobal());
